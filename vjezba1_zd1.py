3bc05bc948fdff5a331625b5745d3ed4889194b7
Mathyan <matijan.cvitanovic@outlook.com>
2022-10-23 23:41:16 +0200

    Vjezba 1 Zad 1 pokusaj 1

---
+++ vjezba1_zd1.py
@@ -0,0 +1,9 @@
+broj = 235
+print("Broj {}".format(broj))
+print("Int {}".format(int(broj)))
+print("Float {}".format(float(broj)))
+print("Bin {}".format(bin(broj)))
+print("Oct {}".format(oct(broj)))
+print("Hex {}".format(hex(broj)))
+print("Bool {}".format(bool(broj)))
+print("String {}".format(str(broj)))
