21ed33867c67faeb3cf28460eb9d506d73d27719
Mathyan <matijan.cvitanovic@outlook.com>
2022-10-24 00:09:02 +0200

    Vjezba 1 Zad 2 pokusaj 1

---
+++ vjezba1_za2.py
@@ -0,0 +1,20 @@
+broj_a = 27
+broj_b = 64
+print("Brojevi su: {}, {}".format(broj_a,broj_b))
+print("{} + {} = {}".format(broj_a,broj_b,broj_a+broj_b))
+print("{} * {} = {}".format(broj_a,broj_b,broj_a*broj_b))
+print("{} / {} = {}".format(broj_a,broj_b,broj_a/broj_b))
+print("{} // {} = {}".format(broj_a,broj_b,broj_a//broj_b))
+print("{} ^ {} = {}".format(broj_a,broj_b,pow(broj_a,broj_b)))
+print("{} % {} = {}".format(broj_a,broj_b,broj_a%broj_b))
+print("{} % {} = {}".format(broj_a,broj_b,broj_a%broj_b))
+broj_a = 27.0
+broj_b = 64.0
+print("Brojevi su: {}, {}".format(broj_a,broj_b))
+print("{} + {} = {}".format(broj_a,broj_b,broj_a+broj_b))
+print("{} * {} = {}".format(broj_a,broj_b,broj_a*broj_b))
+print("{} / {} = {}".format(broj_a,broj_b,broj_a/broj_b))
+print("{} // {} = {}".format(broj_a,broj_b,broj_a//broj_b))
+print("{} ^ {} = {}".format(broj_a,broj_b,pow(broj_a,broj_b)))
+print("{} % {} = {}".format(broj_a,broj_b,broj_a%broj_b))
+print("{} % {} = {}".format(broj_a,broj_b,broj_a%broj_b))
